44a5a571e8164b16d16173284db22f50d0f95465
Wes Evans <euphwes@gmail.com>
2015-01-21 15:25:32 -0700

    Script now handles one (sub)directory of music at a time, processing in batches. Now places the converted files into a new directory of the same name, with the target format appended.

--- pyAudioConverter.py
+++ pyAudioConverter.py
@@ -14,38 +14,83 @@ def run_command(command, args, shell=False):
     return output_lines
 
 
-def perform_conversion(files, args):
+def perform_conversion(args):
+    """Use ffmpeg to perform music file conversion. The arguments contain a
+    top-level directory, presumably an artist directory, or containing several
+    artist directories. The directories are examined recursively, and the ones 
+    containing music files are considered to be album directories, and those 
+    albums are processed. Those album folders are recreated at the same level as
+    the original album folder, and the target format is appended to the folder 
+    name in parentheses. Each music file in the original album is converted to 
+    the target format, and placed in the newly-created directory.
+
+    For example, consider a directory structure like this, with music files in the
+    deepest directories only:
+
+        /music
+            /Green Day
+                /Kerplunk
+                /Insomniac
+            /Tipper
+                /Puzzle Dust EP
+                /Shatter Box EP
+
+    After processing with a target format of 'ogg', the music files will be converted,
+    and placed in the following directory structure (without altering the original
+    files or directory structure):
+
+        /music
+            /Green Day
+                /Kerplunk (ogg)
+                /Insomniac (ogg)
+            /Tipper
+                /Puzzle Dust EP (ogg)
+                /Shatter Box EP (ogg)"""
 
     ffmpeg_path = run_command('where', ['ffmpeg'], shell=True)
     if 'Could not find files' in ffmpeg_path:
         print("ffmpeg not found.")
         sys.exit(1)
 
-    print('')
-    print('Performing conversion:')
+    print('\nPerforming conversion:')
     print('    Output format: ' + args.format)
-    if args.quality and not args.format == 'flac':
+    if args.quality and not args.format in ['flac','wav']:
         print('    Output quality: ' + args.quality)
-    print('')
 
-    for flac in files:
-        new_file = os.path.splitext(flac)[0] + '.' + args.format
-        ffmpeg_args = ['-i', flac, '-qscale:a', args.quality, new_file]
-        print('Converting: {}'.format(os.path.split(flac)[1]))
-        run_command(ffmpeg_path, ffmpeg_args, shell=True)
+    for music_in_directory in gather_files_from_subdirs(args.directory):
+
+        current_dir = os.path.split(music_in_directory[0])[0]
+        old_album_name = os.path.split(current_dir)[1]
+        new_album_name = '{} ({})'.format(old_album_name, args.format)
+        artist_dir = os.path.split(current_dir)[0]
+        target_dir = os.path.join(artist_dir, new_album_name)
+
+        if not os.path.exists(target_dir):
+            os.makedirs(target_dir)
+
+        print('\nProcessing "{}" into "{}"'.format(old_album_name, new_album_name))
+
+        for music_file in music_in_directory:
+            orig_file_no_ext = os.path.splitext(os.path.split(music_file)[1])[0]
+            new_file_with_ext = '{}.{}'.format(orig_file_no_ext, args.format)
+            new_file_path = os.path.join(target_dir, new_file_with_ext)
+
+            ffmpeg_args = ['-i', music_file, '-qscale:a', args.quality, new_file_path]
+            print('    Converting: {}'.format(orig_file_no_ext))
+            run_command(ffmpeg_path, ffmpeg_args, shell=True)
 
 
-def gather_files(root_directory):
-    """Recursively check every file in a directory, identify the music files,
-    and build a list of the full paths of all those files and return it."""
+def gather_files_from_subdirs(directory):
+    """Generator which yields a list of audio files from every subdirectory in
+    the supplied directory, one subdirectory at a time."""
 
-    all_files = list()
-    for root, dirs, files in os.walk(root_directory):
-        music_exts = ['.flac', '.FLAC', '.wav', '.WAV', '.ogg', '.OGG', '.mp3', '.MP3']
-        flac_files = list(filter(lambda x: os.path.splitext(x)[1] in music_exts, files))
-        flac_files = list(os.path.join(root, flac) for flac in flac_files)
-        all_files.extend(flac_files)
-    return all_files
+    music_exts = ['.flac', '.FLAC', '.wav', '.WAV', '.ogg', '.OGG', '.mp3', '.MP3']
+
+    for root, _, files in os.walk(directory):
+        music_files = list(filter(lambda x: os.path.splitext(x)[1] in music_exts, files))
+        music_files = list(os.path.join(root, f) for f in music_files)
+        if len(music_files) > 0:
+            yield music_files
 
 #-----------------------------------------------------------------------------
 
@@ -58,5 +103,4 @@ args = parser.parse_args()
 # make sure format is lowercase so we don't have to deal with that later
 args.format = args.format.lower()
 
-all_files = gather_files(args.directory)
-perform_conversion(all_files, args)
+perform_conversion(args)
